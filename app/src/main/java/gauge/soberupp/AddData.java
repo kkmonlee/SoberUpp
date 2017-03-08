33c5e944ded35ebbd915feed4ad2263023a777f8
ManavGupta04 <manavgupta@hotmail.co.uk>
2017-03-08 17:58:16 +0000

    Reads the text file

--- app/src/main/java/gauge/soberupp/AddData.java
+++ app/src/main/java/gauge/soberupp/AddData.java
@@ -12,9 +12,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class AddData extends AppCompatActivity {
     DatePickerDialog datePickerDialog;
@@ -25,6 +27,15 @@ public class AddData extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_data);
         Intent intent = getIntent();
+
+        // Sets the text box to todays date
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        TextView tv1= (TextView) findViewById(R.id.setDate);
+        tv1.setText(day + "-" + month + "-" + year);
+
     }
 
     public void getData(View view){
@@ -35,13 +46,17 @@ public class AddData extends AppCompatActivity {
 
         // Clears the input data from the text boxes
         unitsDrank.setText("");
-        selectedDate.setText("");
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        selectedDate.setText(day + "-" + month + "-" + year);
 
         Alcohol alcohol = new Alcohol(units, date);
         alcohols.add(alcohol);
 
         objectToString(alcohol);
-        //Snackbar.make(view, Integer.toString(alcohols.size()), Snackbar.LENGTH_LONG).setAction("Action", null).show();
+        Snackbar.make(view, Integer.toString(alcohols.size()), Snackbar.LENGTH_LONG).setAction("Action", null).show();
 
 
         /*System.out.println(date);
@@ -59,12 +74,29 @@ public class AddData extends AppCompatActivity {
             System.out.println("File path => " + AddData.this.getFilesDir().getAbsolutePath());
             outputStream.close();
             System.out.println("HELLO");
+            readFile();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    private void readFile(){
+        FileInputStream fis;
+        try {
+            fis = openFileInput("data.txt");
+            StringBuffer fileContent = new StringBuffer("");
+
+            byte[] buffer = new byte[1024];
+            int n;
+            while ((n = fis.read(buffer)) != -1) {
+                fileContent.append(new String(buffer, 0, n));
+            }
+            System.out.println(fileContent.toString());
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
     public void showDatePickerDialog(View v) {
         DialogFragment newFragment = new DatePickerFragment();
         newFragment.show(getSupportFragmentManager(), "datePicker");
