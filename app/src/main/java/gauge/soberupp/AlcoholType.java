1451bcc81b11c96a5f5df8f7639daefc01c2dda2
Atul <kkmonlee@gmail.com>
2017-03-16 17:44:37 +0000

    Removed ABV field

--- app/src/main/java/gauge/soberupp/AlcoholType.java
+++ app/src/main/java/gauge/soberupp/AlcoholType.java
@@ -7,25 +7,9 @@ package gauge.soberupp;
 public enum AlcoholType {
     BEER("Beer"), CIDER("Cider"), WINE("Wine"), SPIRITS("Spirits");
     private String name;
-    private double abv;
 
     AlcoholType(String name) {
         this.name = name;
-        switch (name) {
-            case "Beer":
-            case "Cider":
-                this.abv = 4.5;
-                break;
-            case "Wine":
-                this.abv = 12;
-                break;
-            case "Spirits":
-                this.abv = 37.5;
-                break;
-            default:
-                this.abv = 4;
-                break;
-        }
     }
 
     public String getName() {
@@ -34,13 +18,5 @@ public enum AlcoholType {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public double getAbv() {
-        return abv;
-    }
-
-    public void setAbv(double abv) {
-        this.abv = abv;
     }
 }
